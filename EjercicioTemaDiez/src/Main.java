104c0c33b07428f59690f00fd7feae32afb85723
EduGarciaVe <edugarcia.venegas@gmail.com>
2023-02-08 13:01:49 -0300

    Ejercicio 7 Temario 10

--- EjercicioTemaDiez/src/Main.java
+++ EjercicioTemaDiez/src/Main.java
@@ -1,30 +1,21 @@
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.Scanner;
 import java.util.Vector;
 
 public class Main {
 
     /**
      *  public static String reverse(String texto) { }
-     *
      * Escribe el código que devuelva una cadena al revés. Por ejemplo, la cadena "hola mundo", debe retornar "odnum aloh".
-     *
      * 1. Crea un array unidimensional de Strings y recórrelo, mostrando únicamente sus valores.
-     *
      * 2. Crea un array bidimensional de enteros y recórrelo, mostrando la posición y el valor de cada elemento en ambas dimensiones.
-     *
      * 3. Crea un "Vector" del tipo de dato que prefieras, y añádele 5 elementos. Elimina el 2o y 3er elemento y muestra el resultado final.
-     *
      * 4. Indica cuál es el problema de utilizar un Vector con la capacidad por defecto si tuviésemos 1000 elementos para ser añadidos al mismo.
-     *
      * 5. Crea un ArrayList de tipo String, con 4 elementos. Cópialo en una LinkedList. Recorre ambos mostrando únicamente el valor de cada elemento.
-     *
      * 6. Crea un ArrayList de tipo int, y, utilizando un bucle rellénalo con elementos 1..10. A continuación, con otro bucle, recórrelo y elimina los numeros pares. Por último, vuelve a recorrerlo y muestra el ArrayList final. Si te atreves, puedes hacerlo en menos pasos, siempre y cuando cumplas el primer "for" de relleno.
-     *
      * 7. Crea una función DividePorCero. Esta, debe generar una excepción ("throws") a su llamante del tipo ArithmeticException que será capturada por su llamante (desde "main", por ejemplo). Si se dispara la excepción, mostraremos el mensaje "Esto no puede hacerse". Finalmente, mostraremos en cualquier caso: "Demo de código".
-     *
      * 8. Utilizando InputStream y PrintStream, crea una función que reciba dos parámetros: "fileIn" y "fileOut". La tarea de la función será realizar la copia del fichero dado en el parámetro "fileIn" al fichero dado en "fileOut".
-     *
      * 9. Sorpréndenos creando un programa de tu elección que utilice InputStream, PrintStream, excepciones, un HashMap y un ArrayList, LinkedList o array.
      *
      */
@@ -117,5 +108,26 @@ public class Main {
         System.out.println("Se eliminaron los numeros pares");
 
         System.out.println("El ArrayList quedó Finalmente sin pares con los siguientes valores :"+ numero);
+
+        System.out.println("----------------");
+        //7. Crea una función DividePorCero. Esta, debe generar una excepción ("throws") a su llamante del tipo ArithmeticException que será capturada por su llamante (desde "main", por ejemplo).
+        // Si se dispara la excepción, mostraremos el mensaje "Esto no puede hacerse". Finalmente, mostraremos en cualquier caso: "Demo de código".
+        DividePorCero(6,0);
+        DividePorCero(6,2);
+
+
+        }
+
+        public static int DividePorCero(int a, int b) throws ArithmeticException{
+            int result =0;
+            try {
+                result= a / b;
+
+            } catch (ArithmeticException e){
+                System.out.println("Esto no puede hacerse");
+            }finally {
+                System.out.println("Demo de código");
+            }
+            return result;
         }
     }
